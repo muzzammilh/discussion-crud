359e0fd61f2eaada23aac076dbb4feff96fefab7
Muzzammil Hussain <muzzammilz@gmail.com>
2021-10-25 18:58:57 +0500

    Added basic get view for posts

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
+
+from .models import Post
+from .serializers import PostSerializer
 
 class PostListApiView(APIView):
     # add permission to check if user is authenticated
@@ -12,6 +16,6 @@ class PostListApiView(APIView):
         '''
         List all the todo items for given requested user
         '''
-        # todos = Todo.objects.filter(user = request.user.id)
-        # serializer = TodoSerializer(todos, many=True)
-        return Response({"dist": "hello world"}, status=status.HTTP_200_OK)
+        posts = Post.objects.all().order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- base/urls.py
+++ base/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
 ]
