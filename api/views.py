b5d02a3c692d9160fe801a3c891c391077b6dc2c
Muzzammil Hussain <muzzammilz@gmail.com>
2021-10-25 22:22:42 +0500

    Added auth and basic settings around it

--- api/views.py
+++ api/views.py
@@ -1,16 +1,11 @@
-from rest_framework import serializers
-from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import permissions
 
 from .models import Post
 from .serializers import PostSerializer
 
 class PostListApiView(APIView):
-    # add permission to check if user is authenticated
-    # permission_classes = [permissions.IsAuthenticated]
 
     # 1. List all
     def get(self, request):
@@ -19,6 +14,7 @@ class PostListApiView(APIView):
         '''
         posts = Post.objects.all().order_by('created_at')
         serializer = PostSerializer(posts, many=True)
+        
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     #2. Create New
--- base/urls.py
+++ base/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib import admin
 
+from rest_framework.authtoken.views import obtain_auth_token  
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+    path('auth/', obtain_auth_token),  
 ]
