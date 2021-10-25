9a6b08548511801ebb7613f988cc40cde4e74857
Muzzammil Hussain <muzzammilz@gmail.com>
2021-10-25 21:56:11 +0500

    Added basic post data endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -14,8 +15,24 @@ class PostListApiView(APIView):
     # 1. List all
     def get(self, request):
         '''
-        List all the todo items for given requested user
+        List all the post items
         '''
-        posts = Post.objects.all().order_by('-created_at')
+        posts = Post.objects.all().order_by('created_at')
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    #2. Create New
+    def post(self, request):
+        '''
+        Add new post item
+        '''
+        data = {
+            'user': request.user.id,
+            'body': request.data.get('body')
+        }
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
